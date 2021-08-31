f5b0da4104e87341c5dc84defd88b6ec8f71d967
Haatom <goldom1730@gmail.com>
2021-08-31 18:18:54 -0500

    Se agregan pruebas y se realiza cambio del POM

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/RegistryTest.java
@@ -1,21 +1,47 @@
 package edu.eci.cvds.tdd.registry;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RegistryTest {
-
     private Registry registry = new Registry();
 
     @Test
     public void validateRegistryResult() {
 
-        Person person = new Person();
-
+        Person person = new Person("Nicolas",3123125,23,Gender.MALE,true);
         RegisterResult result = registry.registerVoter(person);
-
         Assert.assertEquals(RegisterResult.VALID, result);
     }
+    @Test
+    public void validateDuplicate() {
 
-    // TODO Complete with more test cases
+        Person person = new Person("Nicolas",3123125,23,Gender.MALE,true);
+        RegisterResult result1 = registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DUPLICATED, result);
+    }
+
+    @Test
+    public void validateIsDead() {
+
+        Person person = new Person("Juan",2323534,30,Gender.MALE,false);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+
+    @Test
+    public void validateUnderAge() {
+
+        Person person = new Person("David",653234,13,Gender.MALE,true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void validateInvalidAge() {
+
+        Person person = new Person("Ernesto",1343134,-11,Gender.MALE,true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
 }
